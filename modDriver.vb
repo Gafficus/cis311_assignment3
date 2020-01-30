8b35cfd98ee8cf1bb301792b2e2bfdaa729d9919
Nathan Gaffney <nagaffne@svsu.edu>
2020-01-29 20:40:36 -0500

    Problems already lol.

---
+++ modDriver.vb
@@ -0,0 +1,20 @@
+﻿Module modDriver
+
+    Sub Main()
+        configureConsole()
+        Console.WriteLine("Please Enter the path and the name " +
+                           "of the file containing the measurements:")
+        Console.ReadLine()
+        Console.WriteLine("Please enter the path and the name " +
+                          "of the report to generate:")
+        Console.ReadLine()
+    End Sub
+
+    Private Sub configureConsole()
+        Console.ForegroundColor = ConsoleColor.White
+        Console.BackgroundColor = ConsoleColor.Blue
+        Console.WindowWidth = 80
+        Console.WindowHeight = 30
+        Console.Title = "Snape Potion Manufacturing Quality Program"
+    End Sub
+End Module
