85cf3ed807f9076343325aab69a8d505a12538f9
Nathan Gaffney <nagaffne@svsu.edu>
2020-02-04 14:18:24 -0500

    Importing of list and sorting complete.

--- modDriver.vb
+++ modDriver.vb
@@ -1,5 +1,10 @@
 ﻿Module modDriver
-
+    Dim lstTestList As New List(Of udtPotionBottle)
+    Enum enuConstructor
+        intID
+        intVolume
+        strDate
+    End Enum
     Sub Main()
         configureConsole()
         Console.WriteLine("Please Enter the path and the name " +
@@ -8,13 +13,40 @@
         Console.WriteLine("Please enter the path and the name " +
                           "of the report to generate:")
         Console.ReadLine()
+        readInputFile()
+        For Each item As udtPotionBottle In lstTestList
+            Console.WriteLine(item)
+        Next
+        Console.ReadLine()
     End Sub
-
+    Private Sub readInputFile()
+        Dim objMyStreamReader As System.IO.StreamReader
+        Dim strLineContents As String
+        objMyStreamReader = System.IO.File.OpenText("Readings.txt")
+        While Not (objMyStreamReader.EndOfStream)
+            strLineContents = objMyStreamReader.ReadLine()
+            Dim strContents() As String = Split(strLineContents, vbTab)
+            lstTestList.Add(New udtPotionBottle(strContents(enuConstructor.intID),
+                                                strContents(enuConstructor.intVolume),
+                                                strContents(enuConstructor.strDate)))
+        End While
+        lstTestList.Sort(Function(x, y) x.getIntFilledVolume.CompareTo(y.getIntFilledVolume))
+        objMyStreamReader.Close()
+    End Sub
     Private Sub configureConsole()
-        Console.ForegroundColor = ConsoleColor.White
-        Console.BackgroundColor = ConsoleColor.Blue
-        Console.WindowWidth = 80
-        Console.WindowHeight = 30
-        Console.Title = "Snape Potion Manufacturing Quality Program"
+        Dim testObj As Console
+        With testObj
+            .ForegroundColor = ConsoleColor.White
+            .BackgroundColor = ConsoleColor.Blue
+            .WindowWidth = 80
+            .WindowHeight = 30
+            .Title = "Title Here"
+        End With
+        'Console.ForegroundColor = ConsoleColor.White
+        'Console.BackgroundColor = ConsoleColor.Blue
+        'Console.WindowWidth = 80
+        'Console.WindowHeight = 30
+        'Console.Title = "Snape Potion Manufacturing Quality Program"
+        Console.Clear()
     End Sub
 End Module
---
+++ udtPotionBottle.vb
@@ -0,0 +1,41 @@
+﻿Public Class udtPotionBottle
+    Private intID As Integer
+    Private intFilledVolume As Integer
+    Private strDateAsString As String
+    Public Function getIntID()
+        Return intID
+    End Function
+    Public Sub setIntID(ByVal value As Integer)
+        intID = value
+    End Sub
+    Public Function getIntFilledVolume()
+        Return intFilledVolume
+    End Function
+    Public Sub setIntFilledVolume(ByVal value As Integer)
+        intFilledVolume = value
+    End Sub
+    Public Function getStrDateAsString()
+        Return strDateAsString
+    End Function
+    Public Sub setStrDateAsString(ByVal value As String)
+        strDateAsString = value
+    End Sub
+
+    Public Function displayString() As String
+        Return intID & vbTab & intFilledVolume & vbTab & strDateAsString
+    End Function
+    Public Overrides Function ToString() As String
+        Return intID & vbTab & intFilledVolume & vbTab & strDateAsString
+    End Function
+    Public Sub New(intID As Integer,
+                   intFilledVolume As Integer,
+                   strDateAsString As String)
+        If strDateAsString Is Nothing Then
+            Throw New ArgumentNullException(NameOf(strDateAsString))
+        End If
+
+        Me.intID = intID
+        Me.intFilledVolume = intFilledVolume
+        Me.strDateAsString = strDateAsString
+    End Sub
+End Class
